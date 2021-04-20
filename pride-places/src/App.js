c0c10ede76cbe33df8901d4e8ef5be10ba6b2342
Chantel Yip <chantelyip422@gmail.com>
2021-04-19 22:01:25 -0700

    playing around with props

--- pride-places/src/App.js
+++ pride-places/src/App.js
@@ -1,10 +1,43 @@
+import React, { Component } from "react";
+import { CardList } from "./components/Cards/Cards";
 import "./App.css";
-import Cards from "./components/Cards/Cards";
 
-export default function App() {
-  return (
-    <div className="App">
-      <Cards />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          cards: result,
+        });
+      });
+  }
+
+  render() {
+    const { cards } = this.state;
+
+    return (
+      <div className="App">
+        {cards.map((card) => (
+          <div className="flex-container">
+            <CardList name={card.userId}>
+              <h1> poop</h1>
+            </CardList>
+            <p id="post-id"> {card.id}</p>
+            <p id="post-user-id">{card.userId}</p>
+            <p id="post-title">{card.title}</p>
+            <p id="post-body">{card.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
+export default App;
--- pride-places/src/components/Cards/Cards.js
+++ pride-places/src/components/Cards/Cards.js
@@ -1,39 +1,7 @@
 import React, { Component } from "react";
 import "./Cards.css";
 
-class Cards extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((result) => {
-        this.setState({
-          cards: result,
-        });
-      });
-  }
-
-  render() {
-    const { cards } = this.state;
-
-    return (
-      <div className="card">
-        {cards.map((card) => (
-          <div className="flex-container">
-            <p id="post-id"> {card.id}</p>
-            <p id="post-user-id">{card.userId}</p>
-            <p id="post-title">{card.title}</p>
-            <p id="post-body">{card.body}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-export default Cards;
+export const CardList = (props) => {
+  console.log(props);
+  return <div>{props.children}</div>;
+};
