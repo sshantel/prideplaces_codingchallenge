f754c25d606cbb6b5a4b812144436875767a3b03
Chantel Yip <chantelyip422@gmail.com>
2021-04-21 11:58:02 -0700

    added unique key in cards.js

--- pride-places/src/components/Cards/Cards.js
+++ pride-places/src/components/Cards/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Cards.css";
 
 export const CardList = (props) => (
-  <div className="card-list">
+  <div className="card-list key={card.id}">
     {props.cards.map((card) => (
       <h3 key={card.userId}>{card.title}</h3>
     ))}
